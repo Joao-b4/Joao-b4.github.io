f7382047f1239e6027ab4d19799a97e46c726831
root <root@localhost.localdomain>
2018-11-06 21:46:07 -0200

    update 1.3

--- vendor/particles/demo/js/app.js
+++ vendor/particles/demo/js/app.js
@@ -109,7 +109,7 @@ particlesJS('particles-js',
           "speed": 10
         },
         "repulse": {
-          "distance": 200
+          "distance": 50
         },
         "push": {
           "particles_nb": 4
